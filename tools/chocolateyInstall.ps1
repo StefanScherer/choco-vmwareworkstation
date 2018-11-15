503ea10fe8e82ebaa8e5e2d171e38200e6b0c306
Stefan Scherer <scherer_stefan@icloud.com>
2018-11-15 22:49:50 +0100

    Update VMware Workstation 15.0.1

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 $packageName = 'vmwareworkstation'
 $installerType = 'exe'
-$url = 'https://download3.vmware.com/software/wkst/file/VMware-workstation-full-15.0.0-10134415.exe'
+$url = 'https://download3.vmware.com/software/wkst/file/VMware-workstation-full-15.0.1-10737736.exe'
 $url64 = $url
-$checksum = '409598a8ba29119b0aa4856f3486b4e3457e2a19098056f9ce500f02834bff9e'
+$checksum = '2a4c8be9ac1765f31d2b79f4f39b46f3b183bac2723818c46a51ac3d792a6c87'
 $checksum64 = $checksum
 $checksumType = 'sha256'
 $silentArgs = '/s /v/qn EULAS_AGREED=1'
 $validExitCodes = @(0)
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes -checksum $checksum -checksumType $checksumType -checksum64 $checksum64
+Install-ChocolateyPackaggie "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes -checksum $checksum -checksumType $checksumType -checksum64 $checksum64
