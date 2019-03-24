6a70abf3b86e224e2c8ff899dfea2f8d462fc9fc
Stefan Scherer <scherer_stefan@icloud.com>
2019-03-24 21:24:51 +0100

    Cleanup code

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,11 +1,12 @@
-$packageName = 'vmwareworkstation'
-$installerType = 'exe'
-$url = 'https://download3.vmware.com/software/wkst/file/VMware-workstation-full-15.0.3-12422535.exe'
+$ErrorActionPreference = 'Stop'
 
-$checksum = '76c4246edebf98135129a15269618e0778b19419caaac0bffe39884d58ab4407'
-$checksum64 = $checksum
-$checksumType = 'sha256'
-$silentArgs = '/s /v/qn EULAS_AGREED=1'
-$validExitCodes = @(0)
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes -checksum $checksum -checksumType $checksumType -checksum64 $checksum64
+$packageArgs = @{
+  packageName            = 'vmwareworkstation'
+  fileType               = 'exe'
+  url                    = 'https://download3.vmware.com/software/wkst/file/VMware-workstation-full-15.0.3-12422535.exe'
+  checksum               = '76c4246edebf98135129a15269618e0778b19419caaac0bffe39884d58ab4407'
+  checksumType           = 'sha256'
+  silentArgs             = '/s /v/qn EULAS_AGREED=1'
+  validExitCodes         = @(0)
+}
+Install-ChocolateyPackage @packageArgs
