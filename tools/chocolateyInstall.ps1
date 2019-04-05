49b254377b730ad46a66d6f91eecfb43cf39f15a
Stefan Scherer <stefan.scherer@docker.com>
2019-04-05 15:11:23 +0200

    Update VMware Workstation 15.0.4

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -3,10 +3,10 @@ $ErrorActionPreference = 'Stop'
 $packageArgs = @{
   packageName            = 'vmwareworkstation'
   fileType               = 'exe'
-  url                    = 'https://download3.vmware.com/software/wkst/file/VMware-workstation-full-15.0.3-12422535.exe'
-  checksum               = '76c4246edebf98135129a15269618e0778b19419caaac0bffe39884d58ab4407'
+  url                    = 'https://download3.vmware.com/software/wkst/file/VMware-workstation-full-15.0.4-12990004.exe'
+  checksum               = 'e1bae491a1ce559c89b3b836197cddf41e675ba5acf55aa16e86f151c5ee5bca'
   checksumType           = 'sha256'
-  silentArgs             = '/s /v/qn EULAS_AGREED=1'
+  silentArgs             = '/s /v/qn EULAS_AGREED=1 AUTOSOFTWAREUPDATE=0 DATACOLLECTION=0 ADDLOCAL=ALL REBOOT=ReallySuppress'
   validExitCodes         = @(0)
 }
 Install-ChocolateyPackage @packageArgs
