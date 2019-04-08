1fbeaa4b5465679e4e90537e5ee2df2380bc2a53
Alex Lopez <info@ithinkvirtual.com>
2019-04-08 14:59:50 -0400

    Update for VMware Workstation v14.1.7

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@ $ErrorActionPreference = 'Stop'
 $packageArgs = @{
   packageName            = 'vmwareworkstation'
   fileType               = 'exe'
-  url                    = 'https://download3.vmware.com/software/wkst/file/VMware-workstation-full-14.1.6-12368378.exe'
-  checksum               = 'A3ABCCDFE8728120D058F29805AA2794002EBD59E6EA7ACC9BF7CD52ADEF6856'
+  url                    = 'https://download3.vmware.com/software/wkst/file/VMware-workstation-full-14.1.7-12989993.exe'
+  checksum               = 'E927A6547316A55F6CBBCA11E8B74CD97346E92083177AE0E1C148031DCFAF03'
   checksumType           = 'sha256'
   silentArgs             = '/s /v/qn EULAS_AGREED=1 AUTOSOFTWAREUPDATE=0 DATACOLLECTION=0 ADDLOCAL=ALL REBOOT=ReallySuppress'
   validExitCodes         = @(0, 3010)
