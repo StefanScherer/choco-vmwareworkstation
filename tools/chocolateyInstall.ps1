5bfbe8d1dc24312e624b3a7eb376271873840679
Alex Lopez <info@ithinkvirtual.com>
2019-05-14 21:30:39 -0400

    Update for VMware Workstation Pro v15.1.0.13591040

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@ $ErrorActionPreference = 'Stop'
 $packageArgs = @{
   packageName            = 'vmwareworkstation'
   fileType               = 'exe'
-  url                    = 'https://download3.vmware.com/software/wkst/file/VMware-workstation-full-15.0.4-12990004.exe'
-  checksum               = 'e1bae491a1ce559c89b3b836197cddf41e675ba5acf55aa16e86f151c5ee5bca'
+  url                    = 'https://download3.vmware.com/software/wkst/file/VMware-workstation-full-15.1.0-13591040.exe'
+  checksum               = '81B0AF803B7F8E85867FEF5A2450B8E0DE7B09BD105CE96B1AF334581F75FE92'
   checksumType           = 'sha256'
   silentArgs             = '/s /v/qn EULAS_AGREED=1 AUTOSOFTWAREUPDATE=0 DATACOLLECTION=0 ADDLOCAL=ALL REBOOT=ReallySuppress'
   validExitCodes         = @(0, 3010)
