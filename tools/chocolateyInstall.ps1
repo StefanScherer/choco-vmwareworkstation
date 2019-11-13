e51224fe56ed80535045e6cf12cb77e14c621fa1
Alex Lopez <info@ithinkvirtual.com>
2019-11-12 19:52:18 -0500

    Revert "Updated to 15.5.1.15018445"
    This reverts commit 777bfc2f8753ecd2f109321f1bc2e508941abb50.

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@ $ErrorActionPreference = 'Stop'
 $packageArgs = @{
   packageName            = 'vmwareworkstation'
   fileType               = 'exe'
-  url                    = 'https://download3.vmware.com/software/wkst/file/VMware-workstation-full-15.5.1-15018445.exe'
-  checksum               = '1FAB050C8C5EE4C57F63A7EC4FB4F20C7564D8777655FEFFBFA8C47D94146514'
+  url                    = 'https://download3.vmware.com/software/wkst/file/VMware-workstation-full-15.5.0-14665864.exe'
+  checksum               = '07A989FF80B8CF123099ED8AFF86F2A03742EBD3798C4A0F00C94CA74374F63A'
   checksumType           = 'sha256'
   silentArgs             = '/s /v/qn EULAS_AGREED=1 AUTOSOFTWAREUPDATE=0 DATACOLLECTION=0 ADDLOCAL=ALL REBOOT=ReallySuppress'
   validExitCodes         = @(0, 3010)
