1eb67aa86751fb393925001fc9f5e33134998b88
Alex Lopez <info@ithinkvirtual.com>
2020-05-29 09:14:45 -0400

    Updated to 15.5.5.16285975

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@ $ErrorActionPreference = 'Stop'
 $packageArgs = @{
   packageName            = 'vmwareworkstation'
   fileType               = 'exe'
-  url                    = 'https://download3.vmware.com/software/wkst/file/VMware-workstation-full-15.5.2-15785246.exe'
-  checksum               = '393BFD82CCF13BCDA4658072A3C17A1E9277431C45387B417853DE53625FA2AD'
+  url                    = 'https://download3.vmware.com/software/wkst/file/VMware-workstation-full-15.5.5-16285975.exe'
+  checksum               = '661900AB881D413A5DC5993950DA5DA50BD2F3174985F6681E72BF31373945AE'
   checksumType           = 'sha256'
   silentArgs             = '/s /v/qn EULAS_AGREED=1 AUTOSOFTWAREUPDATE=0 DATACOLLECTION=0 ADDLOCAL=ALL REBOOT=ReallySuppress'
   validExitCodes         = @(0, 3010)
